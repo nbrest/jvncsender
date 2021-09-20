f0ac1bb2c8cb601ef89edc9dc7ce3c70b854bcee
nbrest <brest.nico@gmail.com>
2021-09-21 00:41:49 +1000

    Moved tightvnc vncviewer classes to a loadable maven dependency

---
+++ scripts/install-mvn-dependencies.sh
@@ -0,0 +1,5 @@
+
+# Only need to run this once to install the jar into my local-maven-repo folder. 
+# Once it's there, it will be picked up in the pom's dependencies
+
+mvn deploy:deploy-file -DgroupId=com.tightvnc -DartifactId=vncviewer -Dversion=1.3.10 -Durl=file:./local-maven-repo/ -DrepositoryId=local-maven-repo -DupdateReleaseInfo=true -Dfile=./lib/vncviewer-1.3.10-manual-package.jar
